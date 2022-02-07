a6b63fc61e41ad84722df1982bc9b7ae3bfc571a
guiireal <guii_real@hotmail.com>
2022-02-07 00:15:40 -0300

    feat: implement errors

--- src/errors/AppError.ts
+++ src/errors/AppError.ts
@@ -1,8 +1,9 @@
-export class AppError {
+export class AppError extends Error {
   public readonly message: string;
   public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
+    super(message);
     this.message = message;
     this.statusCode = statusCode;
   }
