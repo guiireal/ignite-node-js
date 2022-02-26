aec9561c6fc2383d370dd6dd432d822be29efdbc
guiireal <guii_real@hotmail.com>
2022-02-26 01:47:07 -0300

    feat: add user avatar

--- src/modules/accounts/use_cases/update_user_avatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/use_cases/update_user_avatar/UpdateUserAvatarUseCase.ts
@@ -2,6 +2,7 @@ import { UpdateUserAvatarDTO } from "../../dtos/IUpdateAvatarDTO";
 import { IUseCase } from "../../../../interfaces/IUseCase";
 import { inject, injectable } from "tsyringe";
 import { IUserRepository } from "../../repositories/IUserRepository";
+import { deleteFile } from "../../../../utils/file";
 
 @injectable()
 export class UpdateUserAvatarUseCase implements IUseCase {
@@ -12,6 +13,10 @@ export class UpdateUserAvatarUseCase implements IUseCase {
 
   async execute({ user_id, avatar_file }: UpdateUserAvatarDTO): Promise<void> {
     const user = await this.userRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
     user.avatar = avatar_file;
 
